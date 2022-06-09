9bc5ede4ad6d1421d419cad6f81593590fcaa289
Alain-Zal <63610481+Alain-Zal@users.noreply.github.com>
2022-06-09 17:32:10 +0300

    icn_2

--- mysql-dump/SQLQuery1.sql
+++ mysql-dump/SQLQuery1.sql
@@ -14,8 +14,8 @@ CREATE TABLE icn_user(
 	icn_user_name varchar(20),
 )
 
-DROP TABLE IF EXISTS user_role
-CREATE TABLE user_role(
+DROP TABLE IF EXISTS icn_user_role
+CREATE TABLE icn_user_role(
 	icn_user_id integer not null,
 	icn_role_id integer not null,
 	FOREIGN KEY (icn_user_id) REFERENCES icn_user(icn_user_id),
@@ -23,7 +23,6 @@ CREATE TABLE user_role(
 	)
 
 
-
 INSERT INTO icn_role VALUES ('administrator'),('doctor'),('patient')
 INSERT INTO icn_user VALUES ('Andrei Mladin'),('Radu Baciu'),('Adi Libenciuc'),('Florentina Mirisan'),('Alina Sastras')
-INSERT INTO user_role VALUES (1,1),(1,2),(1,3),(1,3),(1,3)
+INSERT INTO icn_user_role VALUES (1,1),(1,2),(1,3),(1,3),(1,3)
